f3de96613e3b76a1097827f5e0e6609ecd46b392
YONDER\cosminc <cosmin0103@yahoo.com>
2021-02-25 20:03:35 +0200

    model

--- ConsoleApp2/InputModel.cs
+++ ConsoleApp2/InputModel.cs
@@ -8,6 +8,31 @@ namespace ConsoleApp2
 {
     public class InputModel
     {
+        public int DurationOfSimulation = 0;
+        public int NumberOfIntersections;
+        public int NumberOfStreets = 0;
+        public int numberOfCars;
+        public int BonusPoints;
+
+        public List<Street> SteetsInProblem = new List<Street>();
+
+        List<PathOfCar> PathsOfCars = new List<PathOfCar>();
     }
-  
+
+
+    public class Street
+    {
+        public string StreetName;
+        public int StartingIntersection;
+        public int EndingIntersection;
+        public int TimeToCross;
+    }
+
+    public class PathOfCar
+    {
+        public int CardIndexNumber;
+        public int NumberOfStreetsOnCar;
+        public List<Street>  StreetsOnPath;
+    }
+
 }
